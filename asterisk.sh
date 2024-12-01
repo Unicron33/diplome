9c64ebe7b9c7820508eeeb13403981abcec40b4b
Misha Stanilevich <77242179+Unicron33@users.noreply.github.com>
2024-12-01 15:58:23 +0200

    Update asterisk.sh

--- asterisk.sh
+++ asterisk.sh
@@ -1,117 +1,51 @@
 #!/bin/sh
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
+GRAY='\033[0;37m'
 NC='\033[0m' # No Color
 
+
+echo "Running as root..."
+sleep 2
 clear
 
-echo -e "${YELLOW} SIP Server Manager ${NC}"
-echo -e "${YELLOW} 1.${NC} ${CYAN} New SIP User ${NC}"
-echo -e "${YELLOW} 2.${NC} ${CYAN} Delete SIP User ${NC}"
-echo -e "${YELLOW} 3.${NC} ${CYAN} Show Users ${NC}"
-echo -e "${YELLOW} 4.${NC} ${RED} EXIT ${NC}"
-echo ""
 
-read -p " -Enter option number: " choice
+opkg update
 
-case $choice in
-1)
-    read -p " -Enter SIP User (4 digits number): " user
-    read -p " -Enter SIP Password: " pass
-    
-    # Проверка, что пользователь ввёл 4 цифры
-    if echo "$user" | grep -qE '^[0-9]{4}$'; then
-        sleep 1
-    else
-        echo -e "${RED} ERROR: ${user} is not a 4-digit number! ${NC}"
-        sleep 3
-        /sbin/sip.sh
-        exit 1
-    fi
+opkg install asterisk asterisk-pjsip asterisk-bridge-simple asterisk-codec-alaw asterisk-codec-ulaw asterisk-res-rtp-asterisk
 
-    USR=$(grep -o "aors = ${user}" /etc/asterisk/pjsip.conf | grep -o '[[:digit:]]*' | sed -n '1p')
-    sleep 1
+rm /etc/asterisk/extensions.conf
+>/etc/asterisk/pjsip.conf
 
-    if [ "$USR" == "$user" ]; then
-        echo -e "${RED} ERROR: User ${user} already exists ${NC}"
-        sleep 3
-        /sbin/sip.sh
-        exit 1
-    else
-        # Создание нового SIP-пользователя
-        echo "[${user}] ;${user}
-type = endpoint ;${user}
-context = internal ;${user}
-disallow = all ;${user}
-allow = alaw ;${user}
-aors = ${user} ;${user}
-auth = auth${user} ;${user}
-direct_media = no ;${user}
+cd /etc/asterisk/
 
-[${user}] ;${user}
-type = aor ;${user}
-max_contacts = 1 ;${user}
-support_path = yes ;${user}
+wget https://raw.githubusercontent.com/Unicron33/diplome/refs/heads/main/extensions.conf
 
-[auth${user}] ;${user}
-type=auth ;${user}
-auth_type=userpass ;${user}
-password=${pass} ;${user}
-username=${user} ;${user}
+echo "[simpletrans]
+type=transport
+protocol=udp
+bind=0.0.0.0
+
 " >> /etc/asterisk/pjsip.conf
-        
-        echo -e "${GREEN} User ${user} Created Successfully ${NC}"
-    fi
 
-    service asterisk restart
-    sleep 3
-    /sbin/sip.sh
-    ;;
+uci set asterisk.general.enabled='1'
 
-2)
-    read -p " -Enter SIP User to delete: " dele
+service asterisk restart
 
-    # Проверка, что пользователь ввёл 4 цифры
-    if echo "$dele" | grep -qE '^[0-9]{4}$'; then
-        sleep 1
-    else
-        echo -e "${RED} ERROR: ${dele} is not a 4-digit number! ${NC}"
-        sleep 3
-        /sbin/sip.sh
-        exit 1
-    fi
+sleep 1
 
-    PUSR=$(grep -o "aors = ${dele}" /etc/asterisk/pjsip.conf | grep -o '[[:digit:]]*' | sed -n '1p')
-    sleep 1
+service asterisk restart
 
-    if [ "$PUSR" == "$dele" ]; then
-        sed -i "/;$dele/d" /etc/asterisk/pjsip.conf
-        echo -e "${GREEN} User ${dele} Deleted Successfully ${NC}"
-        service asterisk restart
-    else
-        echo -e "${RED} ERROR: User ${dele} does not exist ${NC}"
-    fi
-    sleep 3
-    /sbin/sip.sh
-    ;;
+sleep 1
 
-3)
-    asterisk -rx "pjsip list endpoints"
-    sleep 3
-    /sbin/sip.sh
-    ;;
+service asterisk restart
 
-4)
-    echo -e "${GREEN}Exiting...${NC}"
-    exit 0
-    ;;
+cd
 
-*)
-    echo -e "${RED} Invalid option! ${NC}"
-    sleep 2
-    /sbin/sip.sh
-    ;;
-esac
+rm -f gip.sh && wget https://raw.githubusercontent.com/Unicron33/diplome/refs/heads/main/gip.sh && chmod 777 gip.sh
+
+cp gip.sh /sbin/gip
