31054d0a122794cd86c1e153b9d5748bf9ab13ba
Misha Stanilevich <77242179+Unicron33@users.noreply.github.com>
2024-12-02 15:10:17 +0200

    Update gip.sh

--- gip.sh
+++ gip.sh
@@ -1,56 +1,47 @@
 #!/bin/sh
-# Використання оболонки sh для виконання скрипта
 
-RED='\033[0;31m'      # Визначення кольору для червоного тексту
-GREEN='\033[0;32m'    # Визначення кольору для зеленого тексту
-YELLOW='\033[1;33m'   # Визначення кольору для жовтого тексту
-CYAN='\033[0;36m'     # Визначення кольору для блакитного тексту
-NC='\033[0m'          # Скидання кольору (без кольору)
-
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+ 
 clear
-# Очищення екрану перед виведенням основного меню
 
 echo -e "${YELLOW} SIP Server Manager ${NC}"
-# Виведення заголовка меню
-
-# Виведення пунктів меню
 echo -e "${YELLOW} 1.${NC} ${CYAN} New SIP User ${NC}"
 echo -e "${YELLOW} 2.${NC} ${CYAN} Delete SIP User ${NC}"
 echo -e "${YELLOW} 3.${NC} ${CYAN} Show Users ${NC}"
 echo -e "${YELLOW} 4.${NC} ${RED} EXIT ${NC}"
 echo ""
 
-# Читання вибору користувача
 read -p " -Enter option number: " choice
 
-# Обробка вибору користувача
 case $choice in
 1)
-    # Створення нового SIP користувача
-    read -p " -Enter SIP User (4 digits number): " user  # Введення імені користувача
-    read -p " -Enter SIP Password: " pass               # Введення пароля для користувача
+    read -p " -Enter SIP User (4 digits number): " user
+    read -p " -Enter SIP Password: " pass
     
-    # Перевірка, чи введено 4 цифри
+    # Проверка, что пользователь ввёл 4 цифры
     if echo "$user" | grep -qE '^[0-9]{4}$'; then
         sleep 1
     else
         echo -e "${RED} ERROR: ${user} is not a 4-digit number! ${NC}"
         sleep 3
-        gip # Повернення до основного меню
+        gip
         exit 1
     fi
 
-    # Перевірка, чи вже існує такий користувач
     USR=$(grep -o "aors = ${user}" /etc/asterisk/pjsip.conf | grep -o '[[:digit:]]*' | sed -n '1p')
     sleep 1
 
     if [ "$USR" == "$user" ]; then
         echo -e "${RED} ERROR: User ${user} already exists ${NC}"
         sleep 3
-        gip # Повернення до основного меню
+        gip
         exit 1
     else
-        # Додавання нового користувача до файлу конфігурації Asterisk
+        
         echo "[${user}] ;${user}
 type = endpoint ;${user}
 context = internal ;${user}
@@ -75,17 +66,15 @@ username=${user} ;${user}
         echo -e "${GREEN} User ${user} Created Successfully ${NC}"
     fi
 
-    # Перезапуск служби Asterisk для застосування змін
     service asterisk restart
     sleep 3
     gip
     ;;
 
 2)
-    # Видалення існуючого SIP користувача
-    read -p " -Enter SIP User to delete: " dele  # Введення імені користувача
+    read -p " -Enter SIP User to delete: " dele
 
-    # Перевірка, чи введено 4 цифри
+   
     if echo "$dele" | grep -qE '^[0-9]{4}$'; then
         sleep 1
     else
@@ -95,12 +84,10 @@ username=${user} ;${user}
         exit 1
     fi
 
-    # Перевірка, чи існує такий користувач
     PUSR=$(grep -o "aors = ${dele}" /etc/asterisk/pjsip.conf | grep -o '[[:digit:]]*' | sed -n '1p')
     sleep 1
 
     if [ "$PUSR" == "$dele" ]; then
-        # Видалення запису про користувача з конфігурації Asterisk
         sed -i "/;$dele/d" /etc/asterisk/pjsip.conf
         echo -e "${GREEN} User ${dele} Deleted Successfully ${NC}"
         service asterisk restart
@@ -112,20 +99,17 @@ username=${user} ;${user}
     ;;
 
 3)
-    # Показ існуючих SIP користувачів
     asterisk -rx "pjsip list endpoints"
     sleep 3
     gip
     ;;
 
 4)
-    # Вихід з програми
     echo -e "${GREEN}Exiting...${NC}"
     exit 0
     ;;
 
 *)
-    # Обробка неправильного вибору
     echo -e "${RED} Invalid option! ${NC}"
     sleep 2
     gip
